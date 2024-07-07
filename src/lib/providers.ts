b5ab07f3e1cd5c47041932cd9a9dbb7df66addf1
Markus Strasser <strasser.ms@gmail.com>
2024-07-07 11:17:43 +0200

    chore: error handling with .env in providers

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -2,12 +2,14 @@ import { createOpenAI } from '@ai-sdk/openai'
 import { createAnthropic } from '@ai-sdk/anthropic'
 import * as dotenv from 'dotenv'
 dotenv.config()
-import { json, type RequestHandler } from '@sveltejs/kit'
 import { ConvexHttpClient } from 'convex/browser'
 
-console.log(process.env, 'env ')
+// console.log(process.env, 'env ')
 const convexKey = process.env.PUBLIC_CONVEX_URL || 'nokey'
 
+if (convexKey === 'nokey') {
+  throw new Error('No convex key found')
+}
 //@ts-ignore
 export const convexClient = new ConvexHttpClient(convexKey)
 
