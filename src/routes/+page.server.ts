cc6229b51abaf57574745eca4d4d6838aa89906f
Justin Ahinon <justiny.ahinon@gmail.com>
2024-07-21 14:34:30 +0300

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,28 +1,41 @@
 import { api } from '$convex/_generated/api.js'
+import { ApplicationExplainer } from '$lib/prompts'
+import Tools from '$lib/tools'
 
-export const load = async ({ locals }) => {}
+const generateSequence = async () => {
+  console.log('Generating new sequence')
+
+  const { SequencePreview } = Tools
+  const prompt = `
+  
+  ${ApplicationExplainer}
+    The user is currently on the home screen and chosing between for new course sequences to learn.
+    The user is displayed card previews for each possible next sequence.
+  
+    ${SequencePreview.prompt}
+    
+    `
+
+  //@ts-ignore
+  const content = await SequencePreview.execute(prompt)
+
+  return [content]
+}
 
 export const actions = {
-  default: async ({ fetch, locals }) => {
+  default: async ({ locals }) => {
     try {
-      const res = await fetch('/api/sequences')
-      const newSequences = await res.json()
-      // Store generated sequences in Convex
-      await Promise.all(
-        newSequences.map(seq =>
-          locals.convexClient.mutation(api.sequences.create, { sequence: seq })
-        )
-      )
-      // sequences = newSequences
+      const newSequences = await generateSequence()
 
-      return {
-        hello: 'world',
-      }
+      await Promise.all(
+        newSequences.map(seq => {
+          seq = { ...seq, interactionsIds: [] }
+
+          return locals.convexClient.mutation(api.sequences.create, { sequence: seq })
+        })
+      )
     } catch (error) {
       console.error('Failed to generate sequences:', error)
-      //   return fail(500, {
-      //     error: 'Failed to generate sequences',
-      //   })
 
       return {
         error: 'Failed to generate sequences',
